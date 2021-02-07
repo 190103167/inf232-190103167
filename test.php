6434ff056f82e7669b7be5304162376293642f04
Raiymbek Aubakirov <190103167@stu.sdu.edu.kz>
2021-02-07 16:14:52 +0600

    echo

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test2</title>
+</head>
+<body>
+    <h1>Test2</h1>
+    <?php
+    $a=5;
+    echo $a;
+    echo "<h4>Web programming back end</h4>";
+    ?>
+</body>
+</html>
